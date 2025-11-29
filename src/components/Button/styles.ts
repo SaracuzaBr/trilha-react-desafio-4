d26e17cf5898826ca1fe1022c39a906436323499
SaracuzaBr <mateussaracuza@gmail.com>
2025-11-29 10:04:39 -0300

    Correção definitiva.

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 
-export const ButtonContainer = styled.button<{ disabled?: boolean; children?: React.ReactNode }>`
+export const ButtonContainer = styled.button<{ 
+  disabled?: boolean; 
+  children?: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}>`
     width: 100%;
     height: 42px;
     background-color: #81259D;
